5663f1b67c0ce904368c0a869df3bc54aca7a43e
InfinixAcademy <infinixacademy98@gmail.com>
2025-09-05 21:08:19 +0000

    logo is too small increase size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -45,15 +45,15 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
+      <div className="container flex h-16 items-center">
         <div className="mr-6 flex items-center">
           <Link href="/" className="flex items-center space-x-2">
             <Image 
               src="https://i.postimg.cc/9MLgBMfX/image-Edited.png" 
               alt="MVS Belgaum Logo" 
-              width={32} 
-              height={32}
-              className="h-8 w-8"
+              width={40} 
+              height={40}
+              className="h-10 w-10"
             />
             <span className="font-bold">MVS Belgaum</span>
           </Link>
