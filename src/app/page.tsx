d38e4d3e90eedf1111e6fa3e2ec2addea529990e
sneha khannukar <infinixacademy98@gmail.com>
2025-09-12 05:18:11 +0000

    ok now apps main page i think i have ristricted page lenth so make it as

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,12 +28,12 @@ export default function Home() {
   }
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1">
         <HomeContent />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
